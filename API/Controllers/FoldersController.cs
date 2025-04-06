cc3aab1363561f194dd4a886c404ef3d0bd07fbd
MatePaic <57683081+MatePaic@users.noreply.github.com>
2025-04-06 12:08:19 +0200

    developed setup of project. connected to database and added endpoints for Folder controller

---
+++ API/Controllers/FoldersController.cs
@@ -0,0 +1,52 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FoldersController(StoreContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Folder>>> GetFolders()
+        {
+            return await context.Folders.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")] // api/products/2
+        public async Task<ActionResult<Folder>> GetFolder(int id)
+        {
+            var folder = await context.Folders.FindAsync(id);
+
+            if (folder == null) return NotFound();
+
+            return folder;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Folder>> CreateFolder(Folder folder)
+        {
+            context.Folders.Add(folder);
+
+            await context.SaveChangesAsync();
+
+            return folder;
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var folder = await context.Folders.FindAsync(id);
+
+            if (folder == null) return NotFound();
+
+            context.Remove(folder);
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Folder> Folders { get; set; }
+    }
+}
